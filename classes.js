4d92cc3cf0ee1c102253f14d1e7c146c5106d02b
Ruthbai <minjoobai@gmail.com>
2018-02-06 13:41:43 +1300

    altering the DOM added 4th div

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -39,3 +40,10 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  // First, we have to find the element:
+  var makeVisible = document.getElementsByClassName('makeVisible')[0]
+
+  // Next, we apply a new CSS class to it:
+  makeVisible.classList.add('visible')
+}
